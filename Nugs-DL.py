cca269ccfe5229d1e75128dc5dce7b81bfeff5ab
Sorrow446 <47045733+Sorrow446@users.noreply.github.com>
2019-07-07 01:44:27 +0100

    ALAC & AAC support.
    - Removed a few unneeded lines as tracks come already tagged with album covers (minus the track total field). This will be fetched from the API and wrote to the track. The already embedded album title tag doesn't contain all of the info it should. The correct string will be fetched from the API and wrote to the track (ex:  07/05/19 Boulder, CO -> 07/05/19 Folsom Field, Boulder, CO).
    
    - Folder structure changed: <albumDir>\<tracks> -> Nugs-DL Downloads\<albumDir>\<tracks>.

--- Nugs-DL.py
+++ Nugs-DL.py
@@ -12,7 +12,8 @@ import platform
 # third party
 import requests
 from mutagen import File
-from mutagen.flac import FLAC, Picture
+from mutagen.mp4 import MP4
+from mutagen.flac import FLAC
 
 def GetOsType():
 	osPlatform = platform.system()
@@ -39,7 +40,10 @@ def osCommands(x):
 			sys.stdout.write("\x1b]2;Nugs-DL R1 (by Sorrow446)\x07")
 
 def cleanJson(jsonf):
-	return json.loads(jsonf.rstrip()[21:-2])			
+	try:
+		return json.loads(jsonf.rstrip()[21:-2])
+	except json.decoder.JSONDecodeError:
+		return json.loads(jsonf.rstrip()[20:-2])
 			
 def login(email, pwd):
 	loginGetReq = session.get(f"https://streamapi.nugs.net/secureapi.aspx?orgn=nndesktop&callback=angular.callbacks._3&method=user.site.login&pw={pwd}&username={email}")
@@ -60,20 +64,25 @@ def fetchSubInfo():
 	else:
 		print(f"Signed in successfully - {cleanJson(subInfoGetReq.text)['Response']['subscriptionInfo']['planName'][9:]} account\n")
 		
-def fetchTrackUrl(trackId):
-	trackUrlgetResp = session.get(f"https://streamapi.nugs.net/bigriver/subplayer.aspx?orgn=nndesktop&HLS=1&callback=angular.callbacks._h&platformID=1&trackID={trackId}")
+def fetchTrackUrl(trackId, x):
+	trackUrlgetResp = session.get(f"https://streamapi.nugs.net/bigriver/subplayer.aspx?orgn=nndesktop&HLS=1&callback=angular.callbacks._h&platformID={x}&trackID={trackId}")
 	if trackUrlgetResp.status_code != 200:
 		print(f"Failed to fetch track URL. Response from API: {trackUrlgetResp.text}")
 		osCommands("p")
-	else:		
+	else:
 		return cleanJson(trackUrlgetResp.text)["streamLink"]
 
-def wrap(trackTitle, trackNum, trackTotal):
+def wrap(trackTitle, trackNum, trackTotal, x):
 	def reporthook(blocknum, blocksize, totalsize):
+		if quality == "1":
+			l = f"Downloading track {trackNum} of {trackTotal}: {trackTitle} - 16-bit / 44.1 kHz FLAC"
+		elif quality == "2":
+			l = f"Downloading track {trackNum} of {trackTotal}: {trackTitle} - ALAC"
+		else:
+			l = f"Downloading track {trackNum} of {trackTotal}: {trackTitle} - VBR L4 AAC"
 		readsofar = blocknum * blocksize
 		if totalsize > 0:
 			percent = readsofar * 1e2 / totalsize
-			l = f"Downloading track {trackNum} of {trackTotal}: {trackTitle} - 16-bit / 44.1 kHz FLAC"
 			s = "\r%5.f%%" % (
 			percent)
 			sys.stderr.write(f"{l}{percent:5.0f}%\r")	
@@ -81,72 +90,58 @@ def wrap(trackTitle, trackNum, trackTotal):
 				sys.stderr.write("\n")
 	return reporthook
 
-def fetchTrack(trackUrl, trackTitle, trackNum, trackTotal):
-	urllib.request.urlretrieve(trackUrl, f"{trackNum}.flac", wrap(trackTitle, trackNum, trackTotal))
+def fetchTrack(trackUrl, trackTitle, trackNum, trackTotal, fExt, x):
+	# add error handling.
+	urllib.request.urlretrieve(trackUrl, f"{trackNum}{fExt}", wrap(trackTitle, trackNum, trackTotal, x))
 	
 def fetchMetadata(albumId):
 	metadataGetResp = session.get(f"https://streamapi.nugs.net/api.aspx?orgn=nndesktop&callback=angular.callbacks._4&containerID={albumId}&method=catalog.container&nht=1", verify = False)
-	return cleanJson(metadataGetResp.text)
-	
-def fetchAlbumCov(metaj):
-	albumCovGetResp = requests.get(f"http://api.livedownloads.com{metaj['Response']['pics'][0]['url']}", verify = False)
-	if albumCovGetResp.status_code != 200:
-		print(f"Failed to fetch album cover. Response from API: {albumCovGetResp.text}")
+		if metadataGetResp.status_code != 200:
+		print(f"Failed to fetch metadata. Response from API: {metadataGetResp.text}")
 		osCommands("p")
 	else:
-		if os.path.isfile("cover.jpg"):
-			os.remove("cover.jpg")
-		with open ("cover.jpg", "wb") as f:
-			f.write(albumCovGetResp.content)
+		return cleanJson(metadataGetResp.text)
 	
-def writeFlacTags(file, albumArtist, albumTitle, trackTitle, trackNum, trackTotal):
-	audio = FLAC(file)
-	# write notes from nugs to notes.txt if avail?
-	audio['albumartist'] = albumArtist
-	audio['artist'] = albumArtist
-	audio['album'] = albumTitle
-	audio['title'] = trackTitle
-	audio['tracknumber'] = str(trackNum)
-	audio['tracktotal'] = str(trackTotal)
+def writeTags(file, albumTitle, trackNum, trackTotal):
+	if file.endswith("c"):
+		audio = FLAC(file)
+		audio['album'] = albumTitle
+		audio['tracktotal'] = str(trackTotal)
+	else:
+		audio = MP4(file)
+		audio["\xa9alb"] = albumTitle
+		audio["trkn"] = [(trackNum, trackTotal)]
 	audio.save()
-	
-def writeFlacAlbumCov(file):
-	audio = File(file)
-	image = Picture()
-	image.type = 3
-	mime = "image/jpeg"
-	with open("cover.jpg", "rb") as f:
-		image.data = f.read()
-		audio.clear_pictures()
-		audio.add_picture(image)
-		audio.save()
 
-def renameFiles(trackTitle, trackNum):
+def renameFiles(trackTitle, trackNum, fExt):
 	if not str(trackNum).startswith("0"):
 		if int(trackNum) < 10:
-			finalFilename = f"0{trackNum}. {trackTitle}.flac"
+			finalFilename = f"0{trackNum}. {trackTitle}{fExt}"
 		else:
-			finalFilename = f"{trackNum}. {trackTitle}.flac"
+			finalFilename = f"{trackNum}. {trackTitle}{fExt}"
 	else:
-		finalFilename = f"{trackNum}. {trackTitle}.flac"
+		finalFilename = f"{trackNum}. {trackTitle}{fExt}"
 	if GetOsType():
 		finalFilename = re.sub(r'[\\/:*?"><|]', '-', finalFilename)
 	else:
 		finalFilename = re.sub('/', '-', finalFilename)
 	if os.path.isfile(finalFilename):
 		os.remove(finalFilename)
-	os.rename(f"{trackNum}.flac", finalFilename)
+	os.rename(f"{trackNum}{fExt}", finalFilename)
 
 def albumDirPrep(albumDir):
 	if GetOsType():
 		albumDir = re.sub(r'[\\/:*?"><|]', '-', albumDir)
 	else:
-		albumDir = re.sub('/', '-', albumDir)	
+		albumDir = re.sub('/', '-', albumDir)
+	if not os.path.isdir("Nugs-DL Downloads"):
+		os.mkdir("Nugs-DL Downloads")
+	os.chdir("Nugs-DL Downloads")
 	if not os.path.isdir(albumDir):
-		os.mkdir(albumDir)
+		os.mkdir(albumDir)	
 	os.chdir(albumDir)
 	
-def main():
+def main(quality):
 	url = input("Input Nugs URL: ")
 	try:
 		if not url.strip():
@@ -160,20 +155,27 @@ def main():
 			osCommands("c")
 			metaj = fetchMetadata(url.split('/')[-1])
 			albumArtist = metaj["Response"]["artistName"]
-			albumTitle = metaj['Response']['containerInfo']
+			albumTitle = metaj['Response']['containerInfo'].rstrip()
 			print(f"{albumArtist} - {albumTitle}\n")
 			albumDirPrep(f"{albumArtist} - {albumTitle}")
 			trackTotal = len([x for x in metaj["Response"]["tracks"]])
-			fetchAlbumCov(metaj)
+			if quality == "1":
+				fExt = ".flac"
+				x = "1"
+			elif quality == "2":
+				fExt = ".m4a"
+				x = "0"
+			else:
+				fExt = ".m4a"
+				x = ""
 			i = 0
 			for item in metaj["Response"]["tracks"]:
 				i += 1
-				fetchTrack(fetchTrackUrl(item["trackID"]), item["songTitle"], i, trackTotal)
-				writeFlacTags(f"{i}.flac", albumArtist, albumTitle, item["songTitle"], i, trackTotal)
-				writeFlacAlbumCov(f"{i}.flac")
-				renameFiles(item["songTitle"], i)
+				fetchTrack(fetchTrackUrl(item["trackID"], x), item["songTitle"], i, trackTotal, fExt, x)
+				writeTags(f"{i}{fExt}", albumTitle, i, trackTotal)
+				renameFiles(item["songTitle"], i, fExt)
 			os.remove("cover.jpg")
-			os.chdir("..")
+			os.chdir("....")
 			print("Returning to URL input screen...")
 			time.sleep(1)
 			osCommands("c")
@@ -187,8 +189,8 @@ if __name__ == '__main__':
 	session = requests.session()
 	with open("config.json") as f:
 		config = json.load(f)
-		email, pwd = config["email"], config["password"]
+		email, pwd, quality = config["email"], config["password"], config["quality"]
 	login(email, pwd)
 	fetchSubInfo()
 	while True:
-		main()
+		main(quality)
